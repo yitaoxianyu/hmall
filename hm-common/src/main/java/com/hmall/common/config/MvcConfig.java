df5594f303fb3c0bb9ea271af1631a5337f4b397
XianYu <2861587619@qq.com>
2024-11-25 15:27:49 +0800

    debug(修复了网关模块和knife4j之间的冲突)

--- hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
+++ hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
@@ -2,12 +2,15 @@ package com.hmall.common.config;
 
 
 import com.hmall.common.interceptor.UserInfoInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 //这个配置类需要让每一个服务在启动时都进行添加拦截器，但是每个模块中启动时只会识别启动器同级文件夹所以要将这个写到spring.factories中
+@ConditionalOnClass(DispatcherServlet.class)
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
